26cb702c73388b187ec863ad786e3d5f156e5770
bahatix <113485207+bahatix@users.noreply.github.com>
2024-10-08 22:02:27 -0400

    Update app.js

--- module6-solution/js/app.js
+++ module6-solution/js/app.js
@@ -1,13 +1,21 @@
 // app.js
 
-// Declare the AngularJS module and match it to ng-app declaration
+// Declare the AngularJS module
 var app = angular.module('LunchCheck', []);
 
-// Create a controller within the module
-app.controller('MessageController', function($scope) {
-    $scope.outputMessage = '';
+// Define the controller and inject $scope using the $inject property to prevent minification issues
+app.controller('LunchCheckController', ['$scope', function($scope) {
+    $scope.outputMessage = '';  // Initial empty message
 
+    // Function that updates outputMessage with the content of the input box
     $scope.displayMessage = function() {
-        $scope.outputMessage = $scope.message;
+        if (!$scope.message) {
+            $scope.outputMessage = "Please enter a message!";
+        } else {
+            $scope.outputMessage = $scope.message;
+        }
     };
-});
+}]);
+
+// Alternatively, define the controller using the $inject property explicitly
+app.controller('LunchCheckController').$inject = ['$scope'];

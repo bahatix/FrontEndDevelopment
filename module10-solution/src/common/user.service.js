c74ce8e0db9238fd8af42da6059a48443b858b84
bahatix <113485207+bahatix@users.noreply.github.com>
2024-12-10 22:49:32 -0500

    Create       user.service.js

---
+++ module10-solution/src/common/user.service.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  angular.module('public')
+    .service('UserService', UserService);
+
+  function UserService() {
+    var service = this;
+    var user = null;
+
+    service.saveUser = function (userData) {
+      user = userData;
+    };
+
+    service.getUser = function () {
+      return user;
+    };
+  }
+})();
